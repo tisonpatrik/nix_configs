9bc043f8b0fea7741d9a5f43004e973fb4817b4d
tisonpatrik <patriktison@gmail.com>
2025-05-30 09:38:20 +0200

    first try to multifile config

--- flake.nix
+++ flake.nix
@@ -2,14 +2,21 @@
   description = "Patrik's Nix Flake";
 
   inputs = {
+    # ========================= Core inputs ========================= #
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    # ====================== Home Manager ======================= #
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # ======================= Applications ======================= #
     ghostty = {
       url = "github:ghostty-org/ghostty";
     };
+
+    # ======================== Graphics ========================= #
     nixGL = {
       url = "github:guibou/nixGL";
     };
@@ -17,20 +24,37 @@
 
   outputs = { self, nixpkgs, home-manager, ghostty, nixGL, ... }:
     let
+      # =================== Helper Functions =================== #
       system = "x86_64-linux";
-      pkgs = import nixpkgs { 
+      
+      pkgsFor = system: import nixpkgs {
         inherit system;
+        config = {
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "cursor"
+          ];
+        };
         overlays = [
           ghostty.overlays.default
         ];
       };
-    in {
-      homeConfigurations = {
-        "patrik" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = { inherit nixGL; };
-          modules = [ ./home.nix ];
-        };
+
+      mkHome = system: modules: home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgsFor system;
+        extraSpecialArgs = { inherit nixGL; };
+        modules = modules;
       };
+
+    in {
+      # ================ Home Manager Configurations ================ #
+      homeConfigurations = {
+        "patrik@home" = mkHome system [ ./hosts/home/home.nix ];
+        "patrik@work" = mkHome system [ ./hosts/work/home.nix ];
+        
+        # ====================== Legacy config ====================== #
+        # Keep the original for backward compatibility
+        "patrik" = mkHome system [ ./home.nix ];
+      };
+
     };
 }
---
+++ hosts/work/home.nix
@@ -0,0 +1,108 @@
+{ config, pkgs, nixGL, ... }:
+
+let
+  pythonWithPip = pkgs.python3.withPackages (ps: with ps; [ pip ]);
+in
+{
+  home.username = "patrik";
+  home.homeDirectory = "/home/patrik";
+  home.stateVersion = "24.05";
+  targets.genericLinux.enable = true;
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # Version Control
+    git
+
+    # System Utilities
+    tree
+    direnv
+    flameshot
+    zsh  # Ensure zsh is available
+
+    # Fonts
+    fontconfig
+    dejavu_fonts
+    liberation_ttf
+
+    # Clang
+    clang
+    clang-tools
+    valgrind
+    gdb
+
+    # Go
+    go
+
+    # Python
+    pythonWithPip
+    uv
+
+    # Development Tools
+    lazydocker
+    lazygit
+    
+    # Work-specific tools
+    # docker
+    # awscli2
+
+
+    # Shell Enhancement Tools
+    fzf
+    zoxide
+
+    # Work Apps (minimal personal apps)
+    # signal-desktop  # Commented out for work setup
+
+    # Cursor Editor (with nixGL wrapper)
+    code-cursor
+    (pkgs.writeShellScriptBin "cursor-nixgl" ''
+      export SHELL=${pkgs.zsh}/bin/zsh
+      export NIXOS_OZONE_WL="1"
+      exec ${nixGL.packages.${pkgs.system}.nixGLIntel}/bin/nixGLIntel ${pkgs.code-cursor}/bin/cursor "$@"
+    '')
+
+    # Other Applications with nixGL wrapper
+    (pkgs.writeShellScriptBin "ghostty" ''
+      exec ${nixGL.packages.${pkgs.system}.nixGLIntel}/bin/nixGLIntel ${pkgs.ghostty}/bin/ghostty "$@"
+    '')
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "tisonpatrik";
+    userEmail = "patriktison@gmail.com";  # Consider using work email here
+    extraConfig = {
+      core.editor = "nvim";
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # Zsh configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  # Font configuration
+  fonts.fontconfig.enable = true;
+} 
